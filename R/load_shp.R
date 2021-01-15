62a506c341695e10837fcf52bb4474c8a1ed7b7f
gstephan30 <gstephan30@gmail.com>
2021-01-15 14:25:05 +0100

    added low quality feature in load_shp

--- R/load_shp.R
+++ R/load_shp.R
@@ -4,20 +4,31 @@
 #' and population.
 #'
 #' @param zip_dim integer if zip code dimension has 1, 2, 3 or 5 digits
+#' @param quality character string either "high" (default) or "low"
 #'
 #' @return tibble with geo location, population and zip code
 #'
 #'
 #'
 #' @export
-load_shp <- function(zip_dim){
+load_shp <- function(zip_dim, quality = "high"){
 
   if (!zip_dim %in% c(1, 2, 3, 5)) {
     stop("Please select zip code level 1, 2, 3 or 5")
   }
 
+  if (!quality %in% c("high", "low")) {
+    stop("Please use either high or low as quality")
+  }
+
   layer <- paste0("plz-", zip_dim, "stellig")
-  folder <- paste0(layer, ".shp")
+
+  if (quality == "low") {
+    folder <- paste0(layer, "_mittel.shp")
+  } else if (quality == "high") {
+    folder <- paste0(layer, ".shp")
+  }
+
   path <- here::here("inst", "extdata", folder)
 
   data <- rgdal::readOGR(path, layer = layer)
